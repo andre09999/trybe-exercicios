531977a845f21e3806278cc7c88995b9dbdcff22
Andre <andre09999@live.com>
2022-04-06 15:41:11 -0300

    exercicio let

--- fundamentos/bloco-4-introdução-a-JS/scripts1.js
+++ fundamentos/bloco-4-introdução-a-JS/scripts1.js
@@ -3,3 +3,13 @@ let score = 10.89; // number literal
 let isValid = true;// boolean
 let name; //undefined
 let color = null; //redefinição
+
+let salary = 3500;
+console.log(salary + salary)
+console.log(salary - salary)
+console.log(salary * salary)
+console.log(salary / salary)
+console.log(3 ** 3)
+
+salary++; // Acrescentar 1
+salary--; //tirar 1
