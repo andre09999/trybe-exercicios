80e462c158ccb77d60f4485e1746c43413fb78e3
Andre <andre09999@live.com>
2022-04-06 18:09:16 -0300

    exercicio2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa.js
@@ -1,8 +1,11 @@
 let num1 = 2;
 let num2 = 4;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num1 % num2);
+if (num1 > num2) {
+    console.log(num1,"e maior que",num2)
+}
+else if (num1 === num2) {
+    console.log("Numeros iguais")
+} else {
+    console.log(num2,"e maior que",num1)
+}
