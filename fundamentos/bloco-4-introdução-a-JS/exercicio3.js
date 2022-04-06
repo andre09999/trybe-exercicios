5b582b29adaaf97f6af6108545078dc90d19d504
Andre <andre09999@live.com>
2022-04-06 16:01:56 -0300

    exercicio if/Else

---
+++ fundamentos/bloco-4-introdução-a-JS/exercicio3.js
@@ -0,0 +1,12 @@
+const nota = 80;
+
+if (nota >= 80) {
+    console.log("Parabens, Você Foi Aprovada(o)")
+}
+
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera")
+}
+else {
+    console.log("Você Foi Reprovada(o)")
+}
